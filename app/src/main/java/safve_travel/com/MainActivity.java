ca1a42a17717b3e4e6643051514be01b670cb662
DenserWharf8249 <edgarjrmontero@gmail.com>
2022-05-20 11:07:15 -0500

    confirmacion de github

---
+++ app/src/main/java/safve_travel/com/MainActivity.java
@@ -0,0 +1,14 @@
+package safve_travel.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
